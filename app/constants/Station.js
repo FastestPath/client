4ab4cfbe33ef43f570017168361a9f8c9b95cad4
Tim Bogatchev <tim@ticketevolution.com>
2016-09-26 22:26:39 -0400

    more accurate coordinates

--- app/constants/Station.js
+++ app/constants/Station.js
@@ -2,43 +2,43 @@ export default {
   '33RD_STREET': {
     name: '33rd Street',
     location: {
-      latitude: 40.7485968,
-      longitude: -73.9886298
+      latitude: 40.748541,
+      longitude: -73.988553
     }
   },
   '23RD_STREET': {
     name: '23rd Street' ,
     location: {
-      latitude: 40.742728,
-      longitude: -73.9927608
+      latitude: 40.742738,
+      longitude: -73.99263
     }
   },
   '14TH_STREET': {
     name: '14th Street',
     location: {
-      latitude: 40.7370725,
-      longitude: -73.9973097
+      latitude: 40.737096,
+      longitude: -73.996911
     }
   },
   '9TH_STREET': {
     name: '9th Street',
     location: {
-      latitude: 40.7339915,
-      longitude: -73.9987054
+      latitude: 40.734275,
+      longitude: -73.998713
     }
   },
   'CHRISTOPHER_STREET': {
     name: 'Christopher Street',
     location: {
-      latitude: 40.7330174,
-      longitude: -74.0075243
+      latitude: 40.733047,
+      longitude: -74.007061
     }
   },
   'HOBOKEN': {
     name: 'Hoboken',
     location: {
-      latitude: 40.7350404,
-      longitude: -74.0270187
+      latitude: 40.734942,
+      longitude: -74.027617
     }
   }
 };
